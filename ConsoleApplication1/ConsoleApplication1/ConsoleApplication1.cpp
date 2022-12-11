c40bf3f663aed0989c74f7602ed642edc4daa27a
Nikolas202225 <120285679+Nikolas202225@users.noreply.github.com>
2022-12-11 12:46:08 +0300

    ConsoleApp
    ConsoleApp

---
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    std::cout << "Hello, world!\n";
+
+    std::string source;
+    std::cin >> source;
+
+    std::cout << "source: '" << source << "'\n";
+
+    for (auto i = source.begin(), e = source.end(); i != e; ++i)
+        if (std::count(source.begin(), e, *i) == 1)
+            std::cout << "(";
+        else std::cout << ")";
+
+    std::cout << '\n';
+}
+
+
+
